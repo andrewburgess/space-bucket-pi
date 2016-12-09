5159bc141980f060e45de5fe2ecbe75256fec10e
Andrew Burgess <andrew@andrewburgess.io>
2016-12-08 16:44:39 -0800

    :wrench: chore: update cron schedule to 00 and 30

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = convict({
     cron: {
         doc: 'Cron schedule for capturing images',
         format: String,
-        default: '*/30 * * * *',
+        default: '0/30 * * * *',
         env: 'CRON',
         arg: 'cron'
     },
