3f98e2243c122e480e6c05fa3918d9ea0f9ca4a7
Andrew Burgess <andrew@andrewburgess.io>
2016-12-13 19:01:55 -0800

    :bug: fix: use python application for photocell reading

---
+++ lib/photocell.py
@@ -0,0 +1,17 @@
+import sys
+import RPi.GPIO as GPIO, time, os
+
+GPIO.setmode(GPIO.BOARD)
+
+def readPhotocell(pin):
+    reading = 0;
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+    time.sleep(0.1)
+
+    GPIO.setup(pin, GPIO.IN)
+    while (GPIO.input(pin) == GPIO.LOW):
+        reading += 1
+    return reading
+
+print readPhotocell(int(sys.argv[1]))
