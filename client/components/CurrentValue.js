085a7eb8640548b4fb86001cc0eae460cfc5c70c
Andrew Burgess <andrew@andrewburgess.io>
2019-01-13 12:58:56 -0800

    :recycle: chore: clear out previous code

--- client/components/CurrentValue.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const CurrentValue = styled.h1`
-    font-size: 64px;
-    font-weight: 900;
-    margin: 16px 0;
-`;
-
-export default CurrentValue;
--- client/socket.js
+++
@@ -1,9 +0,0 @@
-import IO from 'socket.io-client';
-
-const socket = IO();
-
-export default function startSocketListener(dispatch) {
-    socket.on('update', (data) => {
-        dispatch(data);
-    });
-}
